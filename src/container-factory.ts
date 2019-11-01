a48bfc63303f4a88496fd521a5652ab2dc945d3a
wesf31 <wes.fukui@gmail.com>
2019-11-01 14:17:09 -0600

    mentor meeting

--- src/container-factory.ts
+++ src/container-factory.ts
@@ -1,8 +1,9 @@
 import { HTTPEvent, IContainer, IContainerFactory } from "@ifit/fleece";
 import { Container } from "inversify";
-import Player, { PlayerBuildFuncType } from "./player";
-import Board, { IBoard } from "./board";
+// import Player, { PlayerBuildFuncType } from "./player";
+// import Board, { IBoard } from "./board";
 import { TYPES } from "./types";
+import { GameDAO, IGameDAO } from "./dao/game-dao";
 
 type ServiceEvent = HTTPEvent;
 
@@ -28,12 +29,16 @@ export class ContainerFactory implements IContainerFactory<ServiceEvent> {
       skipBaseClassChecks: true
     });
 
+  container.bind<IGameDAO>(TYPES.IGameDAO).to(GameDAO);
+
+    /*
     const board = new Board(3);
     container.bind<IBoard>(TYPES.IBoard).toConstantValue(board);
     container.bind<PlayerBuildFuncType>(TYPES.IPlayerBuilder)
       .toConstantValue((id: number, xPos: number, yPos: number, board: IBoard) => 
         Player.Build(id, xPos, yPos, board)
       );
+    */
 
     return container;
   }
--- src/dao/game-dao.ts
+++ src/dao/game-dao.ts
@@ -1,12 +1,23 @@
-import { IGame } from "../game";
+import Game, { IGame } from "../game";
+import { injectable } from "inversify";
+import Board from "../board";
+import Player from "../player";
 
 export interface IGameDAO {
   find(id: string): IGame;
 
 }
 // data access object, takes care
-// 
+@injectable()
 export class GameDAO implements IGameDAO {
+  constructor(){
+    const board1 = new Board(3);
+    const player1 = Player.Build(1, 0, 0, board1);
+    const player2 = Player.Build(2, 1, 0, board1);
+
+    this.games = [];
+    this.games["0"]= new Game(board1, [player1, player2]);
+  }
   /* ? */
   private games: IGame[];
 
@@ -14,4 +25,3 @@ export class GameDAO implements IGameDAO {
     return this.games[id];
   }
 }
-// make sure on master, branch off master, commit and make a PR
