5326e3f8205374a6a2de49dbb3233bcad57c4ca2
wesf31 <wes.fukui@gmail.com>
2019-12-13 13:03:15 -0700

    add: mongodb and mongoose

--- src/dao/game-dao.ts
+++ src/dao/game-dao.ts
@@ -2,17 +2,32 @@ import "reflect-metadata";
 import { IGame } from "../game";
 import { injectable } from "inversify";
 import { store } from "./mock-store";
+import { DataAccessObject, IDataObject } from "@ifit/mongoose-dao";
 
 export interface IGameDAO {
   find(id: string): IGame;
 }
-@injectable()
-export class GameDAO implements IGameDAO {
-  find(id: string): IGame {
-    const game = store.games[id] as IGame;
-    if (!game) {
-      throw new Error(`game of ${id} does not exist`);
-    }
-    return game;
-  }
+
+// export class GameDAO implements IGameDAO {
+//   find(id: string): IGame {
+//     const game = store.games[id] as IGame;
+//     if (!game) {
+//       throw new Error(`game of ${id} does not exist`);
+//     }
+//     return game;
+//   }
+// }
+export interface IGameDO {
+  //DO is the data object which serves the DAO
 }
+@injectable()
+export interface IGameDAO<IGameDO, IGame> extends DataAccessObject<IGameDO, IGame> {
+  // good way to think about a data store is think
+  //hydration/dehydration:
+  find(id: string): Promise<IGameDO>;
+}
+export class GameDAO<IGameDO, IGame> extends DataAccessObject<IGameDO, IGame> implements IGameDAO<IGameDO, IGame> {
+  async find(id: string): Promise<IGameDO> {}
+  //async territory
+}
+// todo get tests back upto date and do async and await study
