a166f9fb8a7de8ae0420ff984561d8b8974ac466
wesf31 <wes.fukui@gmail.com>
2020-04-08 14:48:52 -0600

    seperating monster

--- src/player.test.ts
+++ src/player.test.ts
@@ -1,24 +1,14 @@
-import Player, { IPlayer, Monster, IMonster } from "./player";
+import Player, { IPlayer } from "./player";
 
 describe("player.ts", () => {
   let player: IPlayer;
-  let monster: IMonster;
 
   beforeEach(() => {
     player = Player.Build(2, { x: 0, y: 0 });
-    monster = Monster.Build(4, { x: 0, y: 1 });
   });
   describe("constructor of Player Class", () => {
     it("creates an instance of the Player of the unit class", () => {
       expect(player).toBeInstanceOf(Player);
     });
   });
-  describe("constructor of Monster Class", () => {
-    it("creates an instance of the Monster of the unit class", () => {
-      expect(monster).toBeInstanceOf(Monster);
-    });
-  });
 });
-
-// abstract class testing
-//
