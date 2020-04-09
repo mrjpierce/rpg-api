1a197d8d153aab6402047d7f1f7090979eb4445c
James Pierce <mr.jpierce@gmail.com>
2020-04-09 14:01:24 -0600

    feat: Cleans up game DAO

--- src/dao/game-dao.ts
+++ src/dao/game-dao.ts
@@ -1,34 +1,19 @@
-// import "reflect-metadata";
-// // import Game, { IGame } from "../game";
-// // import { injectable, inject } from "inversify";
-// import { inject } from "inversify";
-// // import { store } from "./mock-store";
-// import { DataAccessObject, IDataObject } from "@ifit/mongoose-dao";
-// // import { types } from "@babel/core";
-// import { TYPES } from "../types";
+import "reflect-metadata";
+import Game, { IGame } from "../game";
+import { injectable, inject } from "inversify";
+import { DataAccessObject } from "@ifit/mongoose-dao";
+import { TYPES } from "../types";
+import { IGameModel } from "../models/game-model";
+import { IGameDO } from "../do/game-do";
 
-// // export interface IGameDAO {
-// //   find(id: string): IGame;
-// // }
+export interface IGameDAO extends DataAccessObject<IGameDO, IGame> {}
 
-// // export class GameDAO implements IGameDAO {
-// //   find(id: string): IGame {
-// //     const game = store.games[id] as IGame;
-// //     if (!game) {
-// //       throw new Error(`game of ${id} does not exist`);
-// //     }
-// //     return game;
-// //   }
-// // }
-// export interface IGameDO extends IDataObject {
-//   //DO is the data object which serves the DAO
-// }
-
-// @injectable()
-// export interface IGameDAO<IGameDO, IGame> extends DataAccessObject<IGameDO, IGame> {}
-// export class GameDAO<IGameDO, IGame> extends DataAccessObject<IGameDO, IGame> implements IGameDAO<IGameDO, IGame> {
-//   constructor(@inject(TYPES.IGameModel) protected gameModel: IGameModel) {
-//     super();
-//     // we are men and men use semicolens;
-//   }
-// }
+@injectable()
+export class GameDAO extends DataAccessObject<IGameDO, IGame> implements IGameDAO {
+  protected targetClass = Game;
+  constructor(
+    @inject(TYPES.IGameModel) protected model: IGameModel
+  ) {
+    super();
+  }
+}
