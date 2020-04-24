48efba97de3c0ebaa114819881a92d6e682e8291
wesf31 <wes.fukui@gmail.com>
2020-04-24 12:02:56 -0600

    fix: deleted monster file

--- src/do/unit-do.ts
+++ src/do/unit-do.ts
@@ -1,7 +1,5 @@
 import { IDataObject } from "@ifit/mongoose-dao";
 
-//switch all names from unit to unit delete unessecary files
-//being more delibrate with work ie defining branches more delibrate, commits more delibrate, creating prs delibrate
 //create branch for boardDO and get setup
 
 export interface IUnitDO extends IDataObject {
