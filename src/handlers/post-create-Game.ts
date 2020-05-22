cb3cd57f0675b7b93fca0f078f55c7e0149f31c2
wesf31 <wes.fukui@gmail.com>
2020-05-22 15:18:48 -0600

    fix: Mentor meetings

--- src/handlers/post-create-Game.ts
+++ src/handlers/post-create-Game.ts
@@ -9,7 +9,13 @@ import { IGameDO } from "../do/game-do";
 import { IGameDAO } from "../dao/game-dao";
 
 export interface IPostGameEvent extends HTTPEvent<IGameDO, null, null> {}
-
+//needs get newGameHandler, won't take any url or body but will have a bearer token in the header that is the users
+//newGameHanlder will handle all the creation of broad and unit
+// bearer token in header not body
+// will save everything and for now send back the gameId of the newcreated game that is saved in the db
+//game will return id of the board, then using that board id and using that to query to get players
+//way to athenticate user, bearer token, in 3t create a user with a bearer token that we can query off of
+//need a way to send info to our client so they can handle it nicely, core package with all our types
 @injectable()
 export class PostGameHandler extends BaseHandler<IGameDO, null, null> {
   constructor(
--- src/models/board-model.ts
+++ src/models/board-model.ts
@@ -7,10 +7,12 @@ initMongoCleanAndTimestamp();
 export type IBoardDoc = IBoardDO & Document;
 export type IBoardModel = Model<IBoardDoc>;
 export const modelName = "Board";
+// what do i actually need to in the database?
 
 export const boardSchema = new Schema({
   gridSize: Number,
   unitGrid: [[{ type: Schema.Types.ObjectId, ref: "UnitGrid" }]],
+  // ref refers to whatever collection
   unitList: [{ type: Schema.Types.ObjectId, ref: "UnitList" }]
 });
 
